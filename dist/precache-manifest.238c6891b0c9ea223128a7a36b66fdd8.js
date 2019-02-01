b3aa2645cb8314fae31435e478488f39a4cc4b14
DianaSolano1 <dianasolano1110@gmail.com>
2019-02-01 02:18:40 -0500

    dist subido

---
+++ dist/precache-manifest.238c6891b0c9ea223128a7a36b66fdd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7ad6fd049486a2f563ef",
+    "url": "/js/chunk-vendors.45cbe53f.js"
+  },
+  {
+    "revision": "47269f8e36c5a9039da9",
+    "url": "/js/chunk-f69ff114.268fecbb.js"
+  },
+  {
+    "revision": "b24a64ada830aa7e19fb",
+    "url": "/js/chunk-0f96df96.6eb09f68.js"
+  },
+  {
+    "revision": "05850b9a67ccb9d8eb8d",
+    "url": "/js/app.d67053a8.js"
+  },
+  {
+    "revision": "df54f9437a0b70cae1176ad86c546904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ad6fd049486a2f563ef",
+    "url": "/css/chunk-vendors.5130b7d2.css"
+  },
+  {
+    "revision": "47269f8e36c5a9039da9",
+    "url": "/css/chunk-f69ff114.63c25e12.css"
+  },
+  {
+    "revision": "b24a64ada830aa7e19fb",
+    "url": "/css/chunk-0f96df96.40b87d96.css"
+  }
+];
